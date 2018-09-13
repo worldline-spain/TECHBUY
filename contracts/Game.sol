8b8987d93ac1a787f8047f10eb176387bd97025c
Raul Fernández <raul.fernandezgarcia@worldline.com>
2018-09-13 17:31:17 +0200

    Game logic

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -8,7 +8,9 @@ contract Game {
     enum Option {
         Rock,
         Paper,
-        Scissors
+        Scissors,
+        Lizard, 
+        Spock
     }
 
     struct Match {
@@ -16,9 +18,9 @@ contract Game {
         address challenging;
         address challenged;
         address winner;
-        uint percentage;
     }
 
+    Match[] matches;
     address pointBankAddress;
     address auctionAddress;
     uint percentage;
@@ -29,12 +31,43 @@ contract Game {
         percentage = _percentage;
     }
 
-    function chooseOption(address _challenged, Option option) public {
-        // currentMatch.options[msg.sender] = option;
-        // count++;
-        // currentMatch.challenging = msg.sender;
-        // currentMatch.challenged = _challenged;
-        // currentMatch.percentage = _percentage;
+    // @dev Game.deployed().then(function(instance){return instance.challenge('0x54F2208866d60768bC4FB5C2f8e0F18f90b60Af7', 0)});
+    function challenge(address _challenged, Option _option) public returns(uint){
+        Match memory m = Match({challenging: msg.sender, challenged: _challenged, winner: msg.sender});
+        uint id = matches.push(m);
+        matches[id].options[msg.sender] = _option;
+        return id;
+    }
+
+    function challengeAccepted(uint _match_id, Option _option) public {
+        Match m = matches[_match_id];
+        m.options[m.challenged] = _option;
+        uint challenging = name_to_number(m.options[m.challenged]);
+        uint challenged = name_to_number(m.options[m.challenging]);
+        if (challenging == challenged) {
+            //play again
+        }
+        else if ((challenging - challenged)%5 < 3) {
+            m.winner = m.challenging;
+        }
+        else {
+            m.winner = m.challenged;
+        }
+    }
+
+    function name_to_number(Option _name) internal pure returns(uint){
+        if (_name == Option.Rock)
+            return 0;
+        else if (_name == Option.Spock)
+            return 1;
+        else if (_name == Option.Paper)
+            return 2;
+        else if (_name == Option.Lizard)
+            return 3;
+        else if (_name == Option.Scissors)
+            return 4;
+        else 
+            return 5;
     }
 
 }
