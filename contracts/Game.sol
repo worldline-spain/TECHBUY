79d8d0e230ce023413c7fd99f5efccd38bcf4e79
Raul Fernández <raul.fernandezgarcia@worldline.com>
2018-09-13 15:28:06 +0200

    Some game logic

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,20 +1,33 @@
 pragma solidity ^0.4.22;
 
 contract Game {
-
-    struct Match {
-        address challenging;
-        address challenged;
-    }
-
+    
     enum Option {
         Rock,
         Paper,
         Scissors
     }
 
-    function chooseOption(Option option) public {
+    struct Match {
+        mapping (address => Option) options;
+        address challenging;
+        address challenged;
+        address winner;
+        uint percentage;
+    }
 
+    uint count = 0;
+    Match currentMatch;
+
+    function chooseOption(Option option) public {
+        currentMatch.options[msg.sender] = option;
+        count++;
+    }
+
+    function startGame(address _challenged, uint _percentage) public {
+        currentMatch.challenging = msg.sender;
+        currentMatch.challenged = _challenged;
+        currentMatch.percentage = _percentage;
     }
 
 }
