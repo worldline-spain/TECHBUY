efdbe0363c58f24785e2fe7562979d468222110d
Dan <dabami7@gmail.com>
2018-12-20 15:54:24 +0100

    WIP - circular transfer history paginated

--- contracts/TransferHistory.sol
+++ contracts/TransferHistory.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract TransferHistory {
+
     
     using SafeMath for uint256;
 
@@ -16,8 +16,8 @@ contract TransferHistory {
     }
 
     mapping (address => Transfer[]) private transfersMap_;
-    mapping (address => uint256) private currentIndexMap_;
-    uint256 private max_ = 5;
+    mapping (address => int256) private currentIndexMap_;
+    int256 private max_ = 6;
 
     function addTransfer(uint256 _amount, string memory _description, uint256 _date, address _from, address _to) public {
         _addTransfer(_amount, _description, _date, _from, _to, _from);
@@ -25,33 +25,34 @@ contract TransferHistory {
     }
     
     function _addTransfer(uint256 _amount, string memory _description, uint256 _date, address _from, address _to, address _reference) private {
-        if (transfersMap_[_reference].length < max_) {
+        if (transfersMap_[_reference].length < uint256(max_)) {
             transfersMap_[_reference].push(Transfer(_amount, _description, _date, _from, _to));
-            currentIndexMap_[_reference] = transfersMap_[_reference].length - 1;
+            currentIndexMap_[_reference] = int256(transfersMap_[_reference].length - 1);
         } else if (currentIndexMap_[_reference] == max_ - 1) {
             currentIndexMap_[_reference] = 0;
-            transfersMap_[_reference][currentIndexMap_[_reference]] = Transfer(_amount, _description, _date, _from, _to);
+            transfersMap_[_reference][uint256(currentIndexMap_[_reference])] = Transfer(_amount, _description, _date, _from, _to);
         } else {
-            currentIndexMap_[_reference] = currentIndexMap_[_reference].add(1);
-            transfersMap_[_reference][currentIndexMap_[_reference]] = Transfer(_amount, _description, _date, _from, _to);
+            currentIndexMap_[_reference]++;
+            transfersMap_[_reference][uint256(currentIndexMap_[_reference])] = Transfer(_amount, _description, _date, _from, _to);
         }
     }
 
-    function getPaginatedTransfers(address _add, uint256 _page, uint256 _resultsPerPage) public view returns (Transfer[] memory) {
+    function getPaginatedTransfers(address _add, int256 _page, int256 _resultsPerPage) public view returns (Transfer[] memory) {
         require(_page > 0 && _page <= max_, "Invalid page.");
         require(_resultsPerPage > 0 && _resultsPerPage <= max_, "Invalid results per page number.");
-        uint256 _transferIndex = _resultsPerPage * _page - _resultsPerPage;
+        int256 _transferIndex = currentIndexMap_[_add] - (_resultsPerPage * (_page - 1));
+        
         Transfer[] memory transfers = transfersMap_[_add];
-        if (transfers.length == 0 || _transferIndex > transfers.length - 1) {
+        if (transfers.length == 0) {
             return;
         }
-        Transfer[] memory _transfers = new Transfer[](_resultsPerPage);
-        uint256 _returnCounter = 0;
-        for (_transferIndex; _transferIndex < _resultsPerPage * _page; _transferIndex++) {
-            if (_transferIndex < transfers.length) {
-                _transfers[_returnCounter] = transfers[_transferIndex];
+        Transfer[] memory _transfers = new Transfer[](uint256(_resultsPerPage));
+        for (int256 i = 0; i < int(_resultsPerPage); i++) {
+            if (i == _transferIndex) {
+                _transfers[uint256(i)] = transfers[0];
+            } else {
+                _transfers[uint256(i)] = transfers[uint256((_transferIndex - i) % max_)];
             }
-            _returnCounter++;
         }
         return _transfers;
     }
